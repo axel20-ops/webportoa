0428cd67efebd49c6cc3ab5a8c6f7d2f0abf4efe
dipoo <77925156+axel20-ops@users.noreply.github.com>
2025-06-21 19:35:26 +0700

    Update script.js

--- assets/script.js
+++ assets/script.js
@@ -12,11 +12,13 @@ window.onload = ketik;
 
 function submitOrder(e) {
   e.preventDefault();
-  const nama = document.getElementById("nama").value;
+  
+  const nama = document.getElementById("nama").value.trim();
   const layanan = document.getElementById("layanan").value;
   const harga = document.getElementById("harga").value;
 
   const pesan = `Halo, saya ${nama} ingin order layanan ${layanan} - ${harga}. Masih tersedia?`;
   const url = `https://wa.me/628557000608?text=${encodeURIComponent(pesan)}`;
+
   window.open(url, "_blank");
 }
