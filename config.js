aba0164fb1e9966aa38a257521e8bb40fdd20f24
dipoo <77925156+axel20-ops@users.noreply.github.com>
2025-09-28 05:59:56 +0700

    Update config.js

--- config.js
+++ config.js
@@ -12,11 +12,12 @@ const SITE = {
   },
   currency: "IDR",
   products: [
-    { number: "++81 80-XX84-8240", country: "Jepang (+81)", price: 45000, status: "available" },
-    { number: "++93 77 1XX 4999", country: "Afghanistan (+93)", price: 35000, status: "available" },
+    { number: "+81 80-XX84-8240", country: "Jepang (+81)", price: 45000, status: "available" },
+    { number: "+93 77 1XX 4999", country: "Afghanistan (+93)", price: 35000, status: "available" },
     { number: "+998 8825555XX", country: "Uzbekistan (+998)", price: 40000, status: "available" },
     { number: "+967 7XX 401 999", country: "Yaman (+967)", price: 35000, status: "available" },
     { number: "+20 11 0X45X222", country: "Mesir (+20)", price: 25000, status: "soldout" }
+    { number: "+852 XX10 9950", country: "Jepang (+81)", price: 45000, status: "available" },
   ],
   lastUpdated: new Date().toISOString()
 };
